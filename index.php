2e58447a97c836d7abaca3435b8b9514c690cc51
gonzjv <gonzjv@ya.ru>
2017-10-09 16:33:47 +0200

    work-09-10-17

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <link href="css/bootstrap.min.css" rel="stylesheet"> 
-        <link href="css/custom.css" rel="stylesheet">
-        <title>Сегодняшняя дата</title>
-    </head>
-    <body>
-        <p>Сегодняшняя дата (согласно данному веб-серверу):
-            <?php
-            echo date('l, F jS Y.');
-            ?>
-        </p>
-    </body>
-</html>
+<?php
+
+if (!isset($_COOKIE['visits']))
+{
+  $_COOKIE['visits'] = 0;
+}
+$visits = $_COOKIE['visits'] + 1;
+setcookie('visits', $visits, time() + 3600 * 24 * 365);
+
+include 'welcome.html.php';
