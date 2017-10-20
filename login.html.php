4cfef72f9a8890cfb2c77957e3a6d26a4853b0cd
gonzjv <gonzjv@ya.ru>
2017-10-20 13:37:24 +0200

    work-2017-10-20

--- login.html.php
+++ login.html.php
@@ -1,11 +1,29 @@
 
-<h1>Log In</h1>
-<p>Please log in to save your history.</p>
+<h4>Войти</h4>
 <?php if (isset($loginError)): ?>
     <p><?php htmlout($loginError); ?></p>
 <?php endif; ?>
 <form action="" method="post">
+    <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+    </div>
+    <div class="form-check">
+        <label class="form-check-label">
+            <input type="checkbox" class="form-check-input">
+            Check me out
+        </label>
+    </div>
     <div>
+        <input type="hidden" name="action" value="login">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+<!--    <div>
         <label for="email">Email: <input type="text" name="email"
                                          id="email"></label>
     </div>
@@ -16,7 +34,7 @@
     <div>
         <input type="hidden" name="action" value="login">
         <input type="submit" value="Log in">
-    </div>
+    </div>-->
 </form>
 <p><a href="/jv/admin/">Return to CMS home</a></p>
 <?php include './logout.inc.html.php'; ?>
