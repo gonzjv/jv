6c604a2ebf1ecbfe75a0d6852ec7c736934b1ba3
gonzjv <gonzjv@ya.ru>
2017-10-24 11:07:36 +0300

    work-2017-10-24

--- login.html.php
+++ login.html.php
@@ -23,18 +23,6 @@
         <input type="hidden" name="action" value="login">
         <button type="submit" class="btn btn-primary">Submit</button>
     </div>
-<!--    <div>
-        <label for="email">Email: <input type="text" name="email"
-                                         id="email"></label>
-    </div>
-    <div>
-        <label for="password">Password: <input type="password"
-                                               name="password" id="password"></label>
-    </div>
-    <div>
-        <input type="hidden" name="action" value="login">
-        <input type="submit" value="Log in">
-    </div>-->
 </form>
 <p><a href="/jv/admin/">Return to CMS home</a></p>
 <?php include './logout.inc.html.php'; ?>
