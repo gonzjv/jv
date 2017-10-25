2df5496a0762ea439087e4c38735f8804a194663
gonzjv <gonzjv@ya.ru>
2017-10-25 15:31:09 +0300

    work-2017-10-25

--- login.html.php
+++ login.html.php
@@ -1,28 +1,26 @@
 
-<h4>Войти</h4>
-<?php if (isset($loginError)): ?>
-    <p><?php htmlout($loginError); ?></p>
-<?php endif; ?>
 <form action="" method="post">
     <div class="form-group">
-        <label for="email">Email address</label>
+        <label for="email">Log In</label>
         <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
     <div class="form-group">
-        <label for="password">Password</label>
+        <label for="password"></label>
         <input type="password" class="form-control" name="password" id="password" placeholder="Password">
     </div>
-    <div class="form-check">
-        <label class="form-check-label">
-            <input type="checkbox" class="form-check-input">
-            Check me out
-        </label>
-    </div>
+    <?php if (isset($loginError)): ?>
+        <p><?php htmlout($loginError); ?></p>
+    <?php endif; ?>
+    <!--    <div class="form-check">
+            <label class="form-check-label">
+                <input type="checkbox" class="form-check-input">
+                Check me out
+            </label>
+        </div>-->
     <div>
         <input type="hidden" name="action" value="login">
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary btn-sm">Log In</button>
+        <a class="float-right" href="./sign_up.html.php">Sign Up</a>
     </div>
 </form>
-<p><a href="/jv/admin/">Return to CMS home</a></p>
-<?php include './logout.inc.html.php'; ?>
