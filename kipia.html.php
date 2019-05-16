172151b9e807f086e9574d56040a6a8fe823cab0
gonzjv <gonzjv@ya.ru>
2019-05-16 16:07:19 +0300

    work-2019-05-16

---
+++ kipia.html.php
@@ -0,0 +1,102 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] .
+        '/includes/helpers.inc.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Цех контрольно-измерительных приборов и автоматики (КИПиА)</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!--<link rel="stylesheet" href="./css/bootstrap.min.css">-->
+        <link rel="stylesheet" href="./css/departments.css">
+    </head>
+    <body>
+        <div class="wrap">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand mx-5" href="./index.php">
+                    <!--<img src="./img/logo_car.png" width="200" height="57" alt="">-->
+                    <div class="lead text-dark">
+                        <strong>
+                            <div>Управление автоматизации</div> 
+                            <div class="mx-4">ОАО "Беларуськалий"</div> 
+                        </strong>
+                    </div>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="navbar-nav lead">
+                        <a class="nav-item nav-link url" href="./index.php">Главная</a>
+                        <a class="nav-item nav-link active url" href="?structure">Структура</a>
+                        <a class="nav-item nav-link url" href="?phones">Телефоны</a>
+                        <!--<a class="nav-item nav-link url" href="?news">Новости</a>-->
+                        <!--<a class="nav-item nav-link url" href="#">Отзывы</a>-->
+                        <!--<a class="nav-item nav-link url" href="#">Галерея</a>-->
+                        <!--<a class="nav-item nav-link url" href="?order">Подать заявку</a>-->
+                        <!--<a class="nav-item nav-link url" href="?shop">Магазин</a>-->
+                    </div>  
+                </div>
+            </nav>
+            <div class="container">
+                <div class="row">
+                    <div class="col-7">
+                        <div class="text-dark mt-3">
+                            <p class="mb-4 text-primary">Цех контрольно-измерительных приборов и автоматики (КИПиА)</p>
+                            <p class="mb-2"><mark>Начальник</mark> – <i>Знатнов Валерий Валерьевич</i>, тел. 29 75 28</p>
+                        </div>
+                    </div>
+                    <div class="col-5 mt-3">
+                        <p class="mb-0"><a class="url" href="?rmin"> Цех ремонта, монтажа и наладки (РМиН)</a></p>
+                        <p class="mb-0"><a class="url" href="?static_lab"> Лаборатория по наладке стационарных установок </a></p>
+                        <p class="mb-0"><a class="url" href="?automation_lab"> Лаборатория автоматизации</a></p>
+                        <p class="mb-0"><a class="url" href="?kipia"> Цех контрольно-измерительных приборов и автоматики (КИПиА)</a></p>
+                        <p class="mb-0"><a class="url" href="?metro_lab"> Лаборатория метрологии</a></p>
+                        <p class="mb-0"><a class="url" href="?radio_lab"> Лаборатория  радиоизотопной  техники  и  радиационной  безопасности (ЛРТиРБ)</a></p>
+                        <p class="mb-0"><a class="url" href="?new_tech_department"> Отдел автоматизации и внедрения новых технологий</a></p>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-7">
+                        <p>В состав цеха контрольно-измерительных приборов и автоматики входит четыре участка и ремонтно-эксплуатационная группа.</p>
+                        <p class="text-info">Участок ремонта и эксплуатации</p>
+                        <p><mark>Начальник</mark> – <i>Вищеня Сергей Александрович</i>, тел. 29 77 11</p>
+                        <ul>
+                            <li>ремонт, подготовка и предъявление в поверку или калибровку средств измерений (температуры, расхода, давления, физико-химического анализа, электрических, радиотехнических и геометрических величин, приборов газовой защиты и газоанализаторов) и испытательного оборудования;</li>
+                            <li>ремонт средств автоматизации – микропроцессорных контроллеров, датчиков, блоков, и многих других приборов;</li>
+                            <li>выполняется большой спектр работ по приему, первичной переработке и сдаче драгметаллов на перерабатывающие предприятия.</li>
+                        </ul>
+                        <p class="text-info">Участок обслуживания и эксплуатации</p>
+                        <p><mark>Начальник</mark> – <i>Корякин Сергей Анатольевич</i>, тел. 29 87 02</p>
+                        <p>Участком обеспечивается техническое обслуживание, подготовка и предъявление в поверку или калибровку средств измерений (температуры, расхода, аналитических физико-химических приборов, вагонных, автомобильных, торговых и аналитических весов), автоматизированных тепловых узлов.</p>
+                        <p class="text-info">Электромеханический участок</p>
+                        <p><mark>Начальник</mark> – <i>Долженков Игорь Витальевич</i>, тел. 29 73 26</p>
+                        <p>Участок занимается монтажем, ремонтом, наладкой и техническим обслуживанием:</p>
+                        <ul>
+                            <li>торгового и холодильного оборудования;</li>
+                            <li>оборудования для приготовления и раздачи пищи в столовых и детских дошкольных учреждениях, автоматов газированной воды;</li>
+                            <li>офисных и промышленных кондиционеров.</li>
+                        </ul>
+                        <p class="text-info">Участок перезарядки огнетушителей и изготовления изделий из пластмасс и других материалов.</p>
+                        <p><mark>Начальник</mark> – <i>Абрамович Игорь Иванович</i>, тел. 29 75 34</p>
+                        <ul>
+                            <li>изготовление изделий из пластмасс (уплотнения для производства конвейерных роликов, бирки для маркировки кабелей, досыльники для проведения взрывных работ и многое другое);</li>
+                            <li>капитальный ремонт и перезарядка огнетушителей.</li>
+                        </ul>
+                        <p class="text-info">Ремонтно-эксплуатационная группа.</p>
+                        <p>Выполняет ремонт и техническое облуживание тепловых и электрических сетей цеха, водопроводного и канализационного хозяйства, технологического оборудования цеха, уборка территории и помещений. </p>
+                    </div>
+                    <div class="col-3">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="./js/jquery-3.3.1.slim.min.js"></script>
+        <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>-->
+        <script src="./js/bootstrap.min.js"></script>
+    </body>
+</html>
